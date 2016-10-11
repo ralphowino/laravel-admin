a1d03ee6b7606fa931325f9dabd0e8b3c6123092
Steve Ops <steve.opiyo5@gmail.com>
2016-10-11 20:08:46 +0300

    Added proper routes

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::get('routes/switch/{theme}', 'Ralphowino\LaravelAdmin\Controllers\LaravelAdminController@switchTheme');
-Route::get('routes/{path?}', 'Ralphowino\LaravelAdmin\Controllers\LaravelAdminController@showPage')
+Route::get('themes/switch/{theme}', 'Ralphowino\LaravelAdmin\Controllers\LaravelAdminController@switchTheme');
+Route::get('themes/{path?}', 'Ralphowino\LaravelAdmin\Controllers\LaravelAdminController@showPage')
     ->where('path', '(.*)');
